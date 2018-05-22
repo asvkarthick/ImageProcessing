941f1111a20ee8b38d926c19fd7c34e1af08b87e
Karthick Kumaran <asvkarthick@gmail.com>
2018-05-21 23:32:43 -0700

    Added more support to the test pattern generator

--- 01_GenerateTestPattern/generate_test_pattern.c
+++ 01_GenerateTestPattern/generate_test_pattern.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 unsigned char *frame_buffer;
 unsigned char *frame_buffer_orig;
@@ -6,13 +7,20 @@ unsigned char *frame_buffer_orig;
 void fillBuffer(unsigned char *buffer, int width, int height)
 {
     int row, col;
+    unsigned char y, u, v;
+
+    y = 0x00;
+    u = 0x80;
+    v = 0x80;
 
     for(row = 0; row < height; row++)
     {
-        for(col = 0; col < width; col++)
+        for(col = 0; col < (width >> 1); col++)
         {
-            *buffer++ = 0x00;
-            *buffer++ = 0x80;
+            *buffer++ = y;
+            *buffer++ = u;
+            *buffer++ = y;
+            *buffer++ = v;
         }
     }
 }
@@ -22,9 +30,20 @@ int main(int argc, char* argv[])
     int width, height, num_frames, frame;
     FILE *fout;
 
-    if(argc < 5)
+    if(argc < 6)
     {
-        fprintf(stderr, "Usage: %s <width> <height> <numFrames> <filename>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <width> <height> <numFrames> <filename> <color>\n", argv[0]);
+        printf("Color can have one of the following values\n");
+        printf("0 - Green (Y = 0x00 U = 0x00 V = 0x00) \n");
+        printf("1 - Red   (Y = 0x00 U = 0x00 V = 0xFF) \n");
+        printf("2 - Blue  (Y = 0x00 U = 0xFF V = 0x00) \n");
+        printf("3 - Purple(Y = 0x00 U = 0xFF V = 0xFF) \n");
+        printf("4 - Green (Y = 0xFF U = 0x00 V = 0x00) \n");
+        printf("5 - Yellow(Y = 0xFF U = 0x00 V = 0xFF) \n");
+        printf("6 - Cyan  (Y = 0xFF U = 0xFF V = 0x00) \n");
+        printf("7 - Pink  (Y = 0xFF U = 0xFF V = 0xFF) \n");
+        printf("8 - Black (Y = 0x00 U = 0x80 V = 0x80) \n");
+        printf("9 - White (Y = 0xFF U = 0x80 V = 0x80) \n");
         return -1;
     }
 
